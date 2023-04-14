1a56882fd1fd08850ddfe22ec86ab34f33b0a79e
Unreal <dodo-pl-08@hotmail.fr>
2023-04-14 14:14:09 +0200

    Added a basic dev command module

---
+++ RaevenBot.Discord/Commands/DevCommandModule.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Serilog;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Local
+
+namespace RaevenBot.Discord.Commands;
+
+[Group("developper"), Aliases("dev", "d")]
+[Description("Various tools for developpers")]
+public class DevCommandModule : BaseCommandModule
+{
+    public ILogger Logger { private get; set; } = null!;
+
+    [Command("guid"), Aliases("uid"), Description("Generates a new Guid (Global Unique Id)")]
+    public async Task GuidGeneratorCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync($"`{Guid.NewGuid()}`");
+    }
+}
